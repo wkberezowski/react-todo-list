c271c69369882e433d7eeb4f636a021a31a5eeea
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-05-19 12:45:28 +0200

    added form and todo components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+import { useState } from 'react';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className='App'>
-      <h1>hello</h1>
+      <header>
+        <h1>Todo List</h1>
+      </header>
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+      />
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,9 @@
+function TodoList() {
+  return (
+    <div className='todo-container'>
+      <ul className='todo-list'></ul>
+    </div>
+  );
+}
+
+export default TodoList;
